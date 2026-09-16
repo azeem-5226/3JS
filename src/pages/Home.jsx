f78d3fd59027454be73a2e22490a8f19e5e55ca8
Azeem <azeemnoor5226@gmail.com>
2026-09-16 14:12:55 +0530

    feat: add Home page component to render portfolio sections and background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,16 @@ import AboutSection from '../sections/AboutSection';
 import SkillsSection from '../sections/SkillsSection';
 import ProjectsSection from '../sections/ProjectsSection';
 import JourneySection from '../sections/JourneySection';
+import ContactSection from '../sections/ContactSection';
+
+const Home = () => {
+  return (
+    <>
+      <ThreeBackground />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
       <JourneySection />
       <ContactSection />
     </>
